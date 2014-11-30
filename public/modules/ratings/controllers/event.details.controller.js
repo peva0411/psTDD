89eb0b29be5ad836d9361fbfa8beeba9433f9941
Phil <peva0411@gmail.com>
2014-11-30 10:45:39 -0500

    added view tests

---
+++ public/modules/ratings/controllers/event.details.controller.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+angular.module('ratings').controller('EventDetailsController',
+['$scope', 'EventsService', controller]);
+
+function controller($scope, EventsService) {
+    $scope.loading = true;
+
+    $scope.getEvent = function(id) {
+        EventsService.getSingleEvent(id)
+            .then(function(data) {
+                $scope.eventDetails = data;
+            })
+            .finally(function() {
+            $scope.loading = false;
+        });
+    };
+}
--- public/modules/ratings/tests/event.list.controller.spec.js
+++ public/modules/ratings/tests/event.list.controller.spec.js
@@ -1,31 +1,31 @@
 ﻿'use strict';
 
 describe('Event List Controller', function() {
-    var EventsService,
-        $rootScope,
+	var EventsService,
+		$rootScope,
 		serviceSpy,
 		deferred,
 		$state,
-        $q,
-        scope;
+		$q,
+		scope;
 
-    beforeEach(module(ApplicationConfiguration.applicationModuleName));
+	beforeEach(module(ApplicationConfiguration.applicationModuleName));
 
-    beforeEach(module(function($provide) {
-        EventsService = {
-            getAllEvents: function() {}
-        };
-        $provide.value('EventsService', EventsService);
-    }));
+	beforeEach(module(function($provide) {
+		EventsService = {
+			getAllEvents: function() {}
+		};
+		$provide.value('EventsService', EventsService);
+	}));
 
 	beforeEach(inject(function(_$rootScope_, $controller, _$q_, $httpBackend, _$state_) {
 		$rootScope = _$rootScope_;
 		scope = $rootScope.$new();
 
-	    $httpBackend.whenGET('modules/core/views/home.client.view.html').respond(200);
+		$httpBackend.whenGET('modules/core/views/home.client.view.html').respond(200);
 
-	    $state = _$state_;
-	    spyOn($state, 'go');
+		$state = _$state_;
+		spyOn($state, 'go');
 
 		$q = _$q_;
 
@@ -47,46 +47,46 @@ describe('Event List Controller', function() {
 		it('Should populate events list from service', function() {
 
 			scope.getAllEvents();
-		    sendDataFromService();
-		    expect(scope.events[0].name).toEqual('event test');
+			sendDataFromService();
+			expect(scope.events[0].name).toEqual('event test');
 		});
 
-	    it('Should set loading to false when data comes back', function() {
+		it('Should set loading to false when data comes back', function() {
 
-	        scope.getAllEvents();
+			scope.getAllEvents();
 
-	        scope.loading = true;
-	        sendDataFromService();
-	        expect(scope.loading).toBeFalsy();
-	    });
+			scope.loading = true;
+			sendDataFromService();
+			expect(scope.loading).toBeFalsy();
+		});
 
-	    it('Should set loading to false when service fails', function() {
-	    	scope.getAllEvents();
-	    	scope.loading = true;
+		it('Should set loading to false when service fails', function() {
+			scope.getAllEvents();
+			scope.loading = true;
 
-	    	deferred.reject();
-	    	$rootScope.$digest();
+			deferred.reject();
+			$rootScope.$digest();
 
-	        expect(scope.loading).toBeFalsy();
-	    });
+			expect(scope.loading).toBeFalsy();
+		});
 	});
 
-    describe('Controller Scope', function() {
-        it('Should initialize loading to true', function() {
-            expect(scope.loading).toBeTruthy();
-        });
-    });
+	describe('Controller Scope', function() {
+		it('Should initialize loading to true', function() {
+			expect(scope.loading).toBeTruthy();
+		});
+	});
 
-    describe('When loading controller', function() {
-        it('Should fetch events', function() {
-            expect(EventsService.getAllEvents).toHaveBeenCalled();
-        });
-    });
+	describe('When loading controller', function() {
+		it('Should fetch events', function() {
+			expect(EventsService.getAllEvents).toHaveBeenCalled();
+		});
+	});
 
-    describe('When selecting an item', function() {
-        it(' Should navigate to the detail state', function() {
-        	scope.selectEvent(1);
-            expect($state.go).toHaveBeenCalledWith('eventDetails', { eventId: 1 });
-        });
-    });
+	describe('When selecting an item', function() {
+		it(' Should navigate to the detail state', function() {
+			scope.selectEvent(1);
+			expect($state.go).toHaveBeenCalledWith('eventDetails', { eventId: 1 });
+		});
+	});
 });
